793be4d7b4c8b87e08d59cd999cee6307946ea1d
Devin Sit <devin@devinsit.com>
2020-01-21 23:06:17 -0500

    Moved the machine learning models folder inside the Backend service so that it can access them without having to be mounted (needed for Cloud Run).

--- webApp/services/backend/src/config.py
+++ webApp/services/backend/src/config.py
@@ -3,7 +3,11 @@ import os
 IMAGE_SIZE = 128
 IMAGES_STORAGE_BUCKET = os.environ.get("IMAGES_STORAGE_BUCKET", "dank-meme-classifier-images-store")
 
-MODELS_LOCATION = os.environ.get("MODEL_STORAGE_LOCATION", "/var/model-storage")
+MODELS_LOCATION = os.environ.get(
+    "MODEL_STORAGE_LOCATION",
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "ml_models")
+)
+
 MODEL_FILE = os.path.join(MODELS_LOCATION, "vgg16_complex_small_images_40_dense_checkpoint.h5")
 
 PROJECT_ID = "devinsit-personal-projects"
