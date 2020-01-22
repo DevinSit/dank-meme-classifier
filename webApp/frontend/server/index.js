0ef2d7c53f131253f201b21dc805b718a27ac4cc
Devin Sit <devin@devinsit.com>
2020-01-22 00:08:28 -0500

    Configured the Frontend server to use the environment variable provided port.

--- webApp/frontend/server/index.js
+++ webApp/frontend/server/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const app = require("./app");
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
     console.log("App listening on port " + PORT + ".");
